a5ef26075d78675013f02cd6b667916497b92e82
shafameidita <99259781+shafameidita@users.noreply.github.com>
2024-08-11 10:24:14 +0700

    Update files

--- Report Monthly Orders Product AGG - Shafa.sql
+++ Report Monthly Orders Product AGG - Shafa.sql
@@ -73,18 +73,31 @@ Penjelasan :
 1. Pada tahap ini saya membuat temporary tabel untuk penjualan seluruh produk yang berstatus Complete di bulan Juli 2024 
 2. Namun karena data yang dipakai yaitu data publik di bigquery, saya tidak bisa membuat tabel baru pada dataset ini sehingga saya hanya melakukan Join pada beberapa tabel untuk mendapatkan data yang sekiranya dibutuhkan untuk mengetahui produk dengan penjualan terbesar pada bulan Juli 2024 
 3. Tabel yang saya gunakan dalam analisis ini yaitu tabel order_items, products, dan users. 
-4. saya melakukan join pada tabel products dan tabel users ke tabel order_items 
+4. SELECT 
+  o.order_id, 
+  o.user_id,
+  CONCAT(u.first_name, ' ', u.last_name) AS user_name,  
+  u.email as user_email, 
+  o.product_id,
+  p.category, 
+  p.name as product_name,
+  p.brand, 
+  o.status, 
+  o.created_at, 
+  o.sale_price
+   - Pada tahap ini saya memilih kolom-kolom dengan nama diatas dari berbagai tabel yang tersedia (contoh: o.order_id adalah kolom order_id pada tabel order_items) 
+   - saya juga menggabungkan kolom (CONCAT) kolom first_name dan last_name dan memberi nama kolom tersebut sebagai kolom user_name yang diakses dari tabel u (users)
+5. saya melakukan join pada tabel products dan tabel users ke tabel order_items 
 (From `bigquery-public-data.thelook_ecommerce.order_items` o : saya mengambil data dari tabel order_items dataset tersebut (sudah dijelaskan sebelumnya) dan saya inisialkan sebagai 'o' untuk memudahkan dan mempersingkat query )
 (JOIN 
   `bigquery-public-data.thelook_ecommerce.products` p = saya menggabungkan tabel products (diinisialkan dengan p) ke tabel orders (o)
 ON 
   o.product_id = p.id) = pada kolom yang sesuai (memiliki nilai yang sama, sama-sama id produk), pada tabel o namanya product_id, dan pada tabel p namanya id
 
-5. saya melakukan hal yang sama juga antara tabel users dan tabel order_items
-
-6. WHERE : ini merupakan tahap filtering
-7. status like 'Complete' : saya mau hanya menampilkan data dengan status Complete
-8. AND o.created_at BETWEEN '2024-07-01' AND '2024-07-31'; : dan saya hanya mau menampilkan pada data bulan Juli 2024
+6. saya melakukan hal yang sama juga antara tabel users dan tabel order_items
+7. WHERE : ini merupakan tahap filtering
+8. status like 'Complete' : saya mau hanya menampilkan data dengan status Complete
+9. AND o.created_at BETWEEN '2024-07-01' AND '2024-07-31'; : dan saya hanya mau menampilkan pada data bulan Juli 2024
 
 
 -- See the Top 10 Products in July 2024 
